d670610bfa9c7cb4051123251c6434cc9494eec0
Dani Guisado <dguisado@hotmail.com>
2019-04-07 17:39:33 +0200

    Update ArduinoDeGo.ino

--- ArduinoDeGo/ArduinoDeGo.ino
+++ ArduinoDeGo/ArduinoDeGo.ino
@@ -1,5 +1,5 @@
 /****************************************************************************
-    Copyright (C) 2004 Alex Shepherd
+    Copyright (C) 2019 Daniel Guisado Serra
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
